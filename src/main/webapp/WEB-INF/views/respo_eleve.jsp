83510e6303e6052b020ba9ec44bbc1333bb5b3b5
Gauth <gauthier.vig@gmail.com>
2016-05-11 03:05:14 +0200

    amélioration de l'input de recherche

--- src/main/webapp/WEB-INF/views/respo_eleve.jsp
+++ src/main/webapp/WEB-INF/views/respo_eleve.jsp
@@ -1,4 +1,5 @@
-<%@ page session="false" contentType="text/html;charset=UTF-8" language="java"  %>
+<%@ page session="false" contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8"  %>
+
 <title>Rechercher des élèves</title>
 <link href="resources/css/respo_eleve.css" rel="stylesheet">
     <div class="container">
--- src/main/webapp/resources/js/respo_eleve.js
+++ src/main/webapp/resources/js/respo_eleve.js
@@ -15,14 +15,37 @@ $("#eleveSearch").keyup(function() {
 });
 $("#eleveSearchc").keyup(function() {
     var val = $.trim(this.value);
-    if (val === "")
-        $rows.show();
+    //Si nul montre tout
+        if (val === "")
+            $rows.show();
 
-    else {
-        $rows.hide();
-        $rows.has("td:contains(" + val + ")").show();
-        //$(".success").show();
-    }
+       
+        //Si c'est le premier mot match
+           
+        else if((val).indexOf(" ")==-1){
+            vall=val;
+            $rows.hide();
+            $rows.has("td:contains(" + vall + ")").show();
+        }
+            //Si c'est le 2ème mots
+        else if((val).indexOf(" ")!=-1) {
+            val=$.trim(val.substring(val.indexOf(" "),val.length));
+            console.log($("tr:contains(bonsoir)").length);
+            $rows.hide();
+            var john=$("tr:contains(John)");
+            console.log($("tr:contains("+vall+")").has("td:contains("+val+")"));
+            $("tr:contains("+vall+")").has("td:contains("+val+")").show();
+            console.log("cc :"+john.has("DoeDoe").length);
+            console.log($rows.has("td:contains("+vall+")"&&"td:contains("+val+")"));
+            console.log(val);
+            console.log(vall);
+            $rows.hide($rows);
+            //$rows.has("td:contains("+vall+")"||"td:contains("+val+")").show();
+            //$rows.has("td:contains(" + val + ")").show();
+        }
+if ((val).indexOf(" ")!=-1){
+}
+
 }
 );
 
