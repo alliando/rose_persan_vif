42b323e52bb47db023293ca01b9e6aae161e6a14
SophieTonnoir <laptitesophie78@hotmail.fr>
2016-05-18 13:41:59 +0200

    LDAP fin + ajout user dans BDD

--- src/main/java/edu/isep/speakisep/LoginController.java
+++ src/main/java/edu/isep/speakisep/LoginController.java
@@ -1,17 +1,12 @@
 package edu.isep.speakisep;
 
-import java.io.IOException;
-import java.sql.ResultSet;
-
 import javax.naming.AuthenticationException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -26,13 +21,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import net.ubilife.spring.customerjdbc.Config;
 import net.ubilife.spring.customerjdbc.User;
 import net.ubilife.spring.customerjdbc.UserManager;
+import net.ubilife.spring.customerjdbc.UserRepository;
+import net.ubilife.spring.customerjdbc.UserRepositoryImpl;
 import edu.isep.speakisep.LDAPObject;
 
 @Controller
 public class LoginController extends HttpServlet {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	@Qualifier("formValidator")
 	private Validator validator;
@@ -50,89 +45,49 @@ public class LoginController extends HttpServlet {
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String submitForm(Model model, @RequestParam("userId") String userId, @RequestParam("password") String password, @Validated User form, BindingResult result) {
+	public String submitForm(Model model, @RequestParam("userId") String userId, @RequestParam("password") String password, @Validated User form, BindingResult result, HttpServletRequest request) {
 		model.addAttribute("form", form);
 		System.out.println( "login : " + userId + " password : " + password );
 		LDAPObject ldap = ISEPAuth( userId , password );
 		 String returnVal = "eleve_home";
+		 
+		 
 		 if(ldap == null) {
 			 returnVal = "form";
 		 }
 		 else{
 			 model.addAttribute("form", form);
-			 returnVal= "eleve_home";
+			 //returnVal= "eleve_home";
+			 String type = ldap.getType(); 
+				
+			HttpSession session = request.getSession();	
+			if(session.isNew()){
+				if (type.equals("eleve")){
+					returnVal= "eleve_profil_modify";
+				} else if ( type.equals("admin") ){
+					returnVal= "admin_home";
+				} else if ( type.equals("respo") ){
+					returnVal= "respo_profil_modify";
+				}
+			}
+			else {
+				if (type.equals("eleve")){
+					returnVal= "eleve_home";
+				} else if ( type.equals("admin") ){
+					returnVal= "admin_home";
+				} else if ( type.equals("respo") ){
+					returnVal= "respo_home";
+				}
+			}
+				
+				 User u1 = new User(ldap.login, ldap.password, ldap.nom, ldap.nomFamille, ldap.prenom, ldap.getType(), ldap.getNumber(), ldap.mail);
+				 AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+				 UserRepository repo = ctx.getBean(UserRepository.class);
+				 repo.save(u1);	
 		 }
 		 return returnVal;
+		 
 	}
-	
-	
-	
-	/*private static final long serialVersionUID = 1L;
-    
-    /**
-     * @see HttpServlet#HttpServlet()
-    
-    public LoginController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		
-		doPost(request,response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
-		
-		String login = request.getParameter("log");
-		String password = request.getParameter("pass");
-		
-		System.out.println( "login : " + login + " password : " + password );
-		
-		LDAPObject result = ISEPAuth( login , password );
-		
-		if ( result == null ){
-			response.sendRedirect("connexion.html");
-			return;
-		}
-		
-		String type = result.getType(); 
-		
-		if ( type.equals("eleve")){
-			response.sendRedirect("accueil_eleve.html");
-		} else if ( type.equals("client") ){
-			response.sendRedirect("accueil_client.html");
-		} else if ( type.equals("tuteur") ){
-			
-		} else if ( type.equals("professeur") ){
-			response.sendRedirect("accueil_prof.html");
-		} else {
-			
-		} 
-		
-		System.out.println(result);
-						
-		String sql = "INSERT INTO User VALUES ('Zara', 'Ali', 18, 'penis')";
-		
-		ResultSet resultSet = Config.sharedInstance().executeSQL(sql);
-		
-		if ( resultSet == null ){
-			System.out.println("prout");
-		}
-		
-		
-	}*/
-	
 	
 	/**
 	 * This method is used to detect if the user is in isep's db
