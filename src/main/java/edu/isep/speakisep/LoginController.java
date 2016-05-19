b2c3f49fc750fff7542aab8cde71665c2f0f94b2
SophieTonnoir <laptitesophie78@hotmail.fr>
2016-05-19 19:07:30 +0200

    changes

--- src/main/java/edu/isep/speakisep/LoginController.java
+++ src/main/java/edu/isep/speakisep/LoginController.java
@@ -29,123 +29,99 @@ import edu.isep.speakisep.LDAPObject;
 @Controller
 public class LoginController extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	@Qualifier("formValidator")
-	private Validator validator;
+private static final long serialVersionUID = 1L;
+@Qualifier("formValidator")
+private Validator validator;
 
-	@InitBinder
-	private void initBinder(WebDataBinder binder) {
-		binder.setValidator(validator);
-	}
-	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String showLoginForm(Model model){
-		User form = new User();
-		model.addAttribute("form", form);
-		return "home";
-	}
-	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String submitForm(Model model, @RequestParam("userId") String userId, @RequestParam("password") String password, @Validated User form, BindingResult result, HttpServletRequest request) {
-		model.addAttribute("form", form);
-		
-		User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-	      String name = user.getNom(); //get logged in username
-	      model.addAttribute("username", name);
-	      
-	      
-		//System.out.println( "login : " + userId + " password : " + password );
-		LDAPObject ldap = ISEPAuth( userId , password );
-		 String returnVal = "eleve_home";
-		 
-		 
-		 if(ldap == null) {
-			 returnVal = "form";
-		 }
-		 else{
-			 model.addAttribute("form", form);
-			 //returnVal= "eleve_home";
-			 String type = ldap.getType(); 
-				
-			HttpSession session = request.getSession();	
-			request.getSession().setAttribute("loggedInUser", user);
-			
-			if(session.isNew()){
-				if (type.equals("eleve")){
-					returnVal= "eleve_profil_modify";
-					//model.addAttribute("eleve", user);
-					request.getSession().setAttribute("eleveLoggedIn", user);
-				} else if ( type.equals("admin") ){
-					returnVal= "admin_home";
-					//model.addAttribute("admin", user);
-					request.getSession().setAttribute("adminLoggedIn", user);
-				} else if ( type.equals("respo") ){
-					returnVal= "respo_profil_modify";
-					//model.addAttribute("respo", user);
-					request.getSession().setAttribute("respoLoggedIn", user);
-				}
-			}
-			else {
-				if (type.equals("eleve")){
-					returnVal= "eleve_home";
-					//model.addAttribute("eleve", user);
-					request.getSession().setAttribute("eleveLoggedIn", user);
-				} else if ( type.equals("admin") ){
-					returnVal= "admin_home";
-					//model.addAttribute("admin", user);
-					request.getSession().setAttribute("adminLoggedIn", user);
-				} else if ( type.equals("respo") ){
-					returnVal= "respo_home";
-					//model.addAttribute("respo", user);
-					request.getSession().setAttribute("respoLoggedIn", user);
-				}
-			}
-				
-				 User u1 = new User(ldap.login, ldap.password, ldap.nom, ldap.nomFamille, ldap.prenom, ldap.getType(), ldap.getNumber(), ldap.mail);
-				 AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
-				 UserRepository repo = ctx.getBean(UserRepository.class);
-				 repo.save(u1);	
-		 }
-		 return returnVal;
-		 
-	}
-	
-	/**
-	 * This method is used to detect if the user is in isep's db
-	 * 
-	 * @param login
-	 * @param password
-	 * @return 
-	 */
-	private LDAPObject ISEPAuth( String login, String password ){
-	
-		LDAPaccess access = new LDAPaccess();
-		try {
-			LDAPObject isepUser = access.LDAPget( login , password ); 
+@InitBinder
+private void initBinder(WebDataBinder binder) {
+binder.setValidator(validator);
+}
+@RequestMapping(value = "/login", method = RequestMethod.GET)
+public String showLoginForm(Model model){
+User form = new User();
+model.addAttribute("form", form);
+return "home";
+}
+@RequestMapping(value = "/login", method = RequestMethod.POST)
+public String submitForm(Model model, @RequestParam("userId") String userId, @RequestParam("password") String password, @Validated User form, BindingResult result, HttpServletRequest request) {
+model.addAttribute("form", form);
 
-		if (isepUser == null)
-		{	
-			System.err.println("user doesn't exist");
-			return null;
-		}
-		    UserManager.sharedInstance().currentUser = this.warpUserModel(isepUser);
-		
-			return isepUser;
-			
-		} catch(Exception e) {
-			
-			if ( e instanceof AuthenticationException ){
-				System.err.println(e.getMessage());
-				return null;
-			}
-			
-			System.err.println(e.getMessage());
-			return null;
-		}
-	}
-	
-	private User warpUserModel (LDAPObject isepUser){
-		return new User(isepUser.getLogin(), isepUser.getPassword(), isepUser.getNom(), isepUser.getNomFamille(), isepUser.getPrenom(), isepUser.getType(), isepUser.getNumber(), isepUser.getMail());
-	}
+LDAPObject ldap = ISEPAuth( userId , password );
+String returnVal = "eleve_home";
+ 
+ 
+if(ldap == null) {
+returnVal = "form";
+}
+else{
+model.addAttribute("form", form);
+//returnVal= "eleve_home";
+String type = ldap.getType(); 
+HttpSession session = request.getSession();
+
+if(session.isNew()){
+if (type.equals("eleve")){
+returnVal= "eleve_profil_modify";
+
+} else if ( type.equals("admin") ){
+returnVal= "admin_home";
+
+} else if ( type.equals("respo") ){
+returnVal= "respo_profil_modify";
+
+}
+}
+else {
+if (type.equals("eleve")){
+returnVal= "eleve_home";
+
+} else if ( type.equals("admin") ){
+returnVal= "admin_home";
+
+} else if ( type.equals("respo") ){
+returnVal= "respo_home";
+
+}
+}
+User u1 = new User(ldap.login, ldap.password, ldap.nom, ldap.nomFamille, ldap.prenom, ldap.getType(), ldap.getNumber(), ldap.mail);
+AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+UserRepository repo = ctx.getBean(UserRepository.class);
+repo.save(u1);
+}
+return returnVal;
+ 
+}
+/**
+* This method is used to detect if the user is in isep's db
+* 
+* @param login
+* @param password
+* @return 
+*/
+private LDAPObject ISEPAuth( String login, String password ){
+LDAPaccess access = new LDAPaccess();
+try {
+LDAPObject isepUser = access.LDAPget( login , password ); 
+
+if (isepUser == null)
+{
+System.err.println("user doesn't exist");
+return null;
+}
+   UserManager.sharedInstance().currentUser = this.warpUserModel(isepUser);
+return isepUser;
+} catch(Exception e) {
+if ( e instanceof AuthenticationException ){
+System.err.println(e.getMessage());
+return null;
+}
+System.err.println(e.getMessage());
+return null;
+}
+}
+private User warpUserModel (LDAPObject isepUser){
+return new User(isepUser.getLogin(), isepUser.getPassword(), isepUser.getNom(), isepUser.getNomFamille(), isepUser.getPrenom(), isepUser.getType(), isepUser.getNumber(), isepUser.getMail());
+}
 
 }
