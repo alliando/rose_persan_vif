e5c2cb35646906dc5ae4a60bb3e45f48e80fb6da
RemiNabonne <remi.nabonne@gmail.com>
2016-05-19 21:29:56 +0200

    Add files via upload

---
+++ src/main/java/edu/isep/speakisep/AdminRespoController.java
@@ -0,0 +1,25 @@
+package edu.isep.speakisep;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class AdminRespoController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AdminRespoController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/admin_respo", method = RequestMethod.GET)
+	public String home() {
+		return "admin_respo";
+	}
+	
+}
