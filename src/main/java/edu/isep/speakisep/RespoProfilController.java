0f9e3cc449c295000b48aae7fd600696ce75ecfb
Gauth <gauthier.vig@gmail.com>
2016-05-27 00:08:24 +0200

    /

--- src/main/java/edu/isep/speakisep/RespoProfilController.java
+++ src/main/java/edu/isep/speakisep/RespoProfilController.java
@@ -1,15 +1,26 @@
 package edu.isep.speakisep;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import net.ubilife.spring.customerjdbc.Config;
+import net.ubilife.spring.customerjdbc.ParcoursRepository;
+import net.ubilife.spring.customerjdbc.User;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class RespoProfilController {
-	@Autowired
 	@RequestMapping("/respo_profil")
 	
-	public String Profil_respo(){
+	public String Profil_respo(	HttpServletRequest request
+	){
+		HttpSession session= request.getSession();
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+		ParcoursRepository repo=ctx.getBean(ParcoursRepository.class);
+		User user=(User)session.getAttribute("user");
+		session.setAttribute("parcours",repo.findOne(user.getIdParcours()));
 		return "respo_profil";
 	}
 }
