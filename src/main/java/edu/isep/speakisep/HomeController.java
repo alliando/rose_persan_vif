db5a88bb8f7e8645a57514511ddfcea3d222dca0
Gauth <gauthier.vig@gmail.com>
2016-06-02 17:07:03 +0200

    test

--- src/main/java/edu/isep/speakisep/HomeController.java
+++ src/main/java/edu/isep/speakisep/HomeController.java
@@ -1,5 +1,7 @@
 package edu.isep.speakisep;
 
+import net.ubilife.spring.customerjdbc.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +11,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
+
 
 		return "home";
 	}
