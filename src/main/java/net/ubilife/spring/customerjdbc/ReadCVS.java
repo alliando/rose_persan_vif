8ee07ad72563f38b3bdd2dbce57175b69a94296a
Gauth <gauthier.vig@gmail.com>
2016-06-19 22:46:02 +0200

    uf8 csv

--- src/main/java/net/ubilife/spring/customerjdbc/ReadCVS.java
+++ src/main/java/net/ubilife/spring/customerjdbc/ReadCVS.java
@@ -50,11 +50,12 @@ public class ReadCVS {
                 br = new BufferedReader(new FileReader(loader.getResource(csvFile).getPath()));
                 String nomParcours=br.readLine().split(";")[1];
                 nomParcours=nomParcours.split("Promo")[0];
-                nomParcours = new String(nomParcours.getBytes("iso-8859-1"), "utf8");
+                //nomParcours = new String(nomParcours.getBytes("iso-8859-1"), "utf8");
 
                 int register=0;
                 for(Parcours parcours:repoP.findAll()){
-                    if (parcours.getNomparcours().equals(nomParcours)){
+                    String getNomParcours=new String(parcours.getNomparcours().getBytes("iso-8859-1"), "utf8");
+                    if (getNomParcours.equals(nomParcours)){
                         register=1;
                         break;
                     }
